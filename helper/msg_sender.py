6b194dfdee4c021207e7502f4357a0692b372f8b
kasfil <kasf@tuta.io>
2024-11-11 23:31:22 +0700

    chore: update send messages function
    this update include fund request job queue

--- helper/msg_sender.py
+++ helper/msg_sender.py
@@ -2,22 +2,33 @@ import logging
 
 from telegram import Update
 from telegram import error as tele_error
-from telegram.constants import ParseMode
 from telegram.ext import ContextTypes
 
-from config import MAX_MSG_CHARS
+from config import FUND_REQ_IDLE, MAX_MSG_CHARS
+from helper import scheduler
 
 
-async def send_messages(sender: Update | ContextTypes.DEFAULT_TYPE, msgs: list[str]) -> None:
+async def send_messages(
+    update: Update | None,
+    context: ContextTypes.DEFAULT_TYPE,
+    msgs: list[str],
+) -> None:
+    chat_id: int
+    if update and update.effective_message:
+        chat_id = update.effective_message.chat_id
+    elif context and context.job and context.job.chat_id:
+        chat_id = context.job.chat_id
+    else:
+        return
+
     for msg in msgs:
         try:
-            if isinstance(sender, Update):
-                await sender.message.reply_markdown_v2(msg)  # type: ignore
+            if update and update.message:
+                await update.message.reply_markdown_v2(msg)
             else:
-                await sender.bot.send_message(
-                    chat_id=sender.job.chat_id,  # type: ignore
+                await context.bot.send_message(
+                    chat_id=chat_id,
                     text=msg,
-                    parse_mode=ParseMode.MARKDOWN_V2,
                 )
         except tele_error.BadRequest as e:
             if e.message == "Message is too long":
@@ -33,12 +44,28 @@ async def send_messages(sender: Update | ContextTypes.DEFAULT_TYPE, msgs: list[s
                         logging.error(msg)
                         break
 
-                    if isinstance(sender, Update):
-                        await sender.message.reply_markdown_v2(msg[current_idx:next_idx].strip())  # type: ignore
+                    if update and update.message:
+                        await update.message.reply_markdown_v2(msg[current_idx:next_idx].strip())
                     else:
-                        await sender.bot.send_message(
-                            chat_id=sender.job.chat_id,  # type: ignore
+                        await context.bot.send_message(
+                            chat_id=chat_id,
                             text=msg[current_idx:next_idx].strip(),
-                            parse_mode=ParseMode.MARKDOWN_V2,
                         )
                     current_idx = next_idx + 1
+
+    if not context.job_queue or FUND_REQ_IDLE <= 0:
+        return
+
+    job_name = f"fund_{chat_id}"
+    current_job_queue = context.job_queue.get_jobs_by_name(job_name)
+
+    for j in current_job_queue:
+        if not j.removed:
+            j.schedule_removal()
+
+    context.job_queue.run_once(
+        scheduler.fund_request,
+        when=float(FUND_REQ_IDLE),
+        chat_id=chat_id,
+        name="fund_" + str(chat_id),
+    )
